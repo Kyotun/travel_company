0e725252437d9e8292304df7a20c0332cba7b862
Kyotun <emirpisirici@hotmail.com>
2024-03-25 16:17:34 +0100

    Coworker class: Added 3 constructors.

--- Coworker.java
+++ Coworker.java
@@ -10,4 +10,23 @@ public class Coworker extends Person{
     public void setUsernumber(String user_number){
         this.user_number= user_number;
     }
+
+    //Constructors
+    public Coworker(String ID){
+        super(ID);
+    }
+
+    public Coworker(String ID, String user_number) {
+        super(ID);
+        this.user_number = user_number;
+    }
+
+    public Coworker(String ID, String name, String surname){
+        super(ID, name, surname);
+    }
+
+    public Coworker(String ID, String name, String surname, String user_number){
+        super(ID, name, surname);
+        this.user_number = user_number;
+    }
 }
