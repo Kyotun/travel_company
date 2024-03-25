32dddb674d4b672bf688a4286ff8007c1bbaf102
Kyotun <emirpisirici@hotmail.com>
2024-03-25 14:49:08 +0100

    Person class: Added 3 constructors, added set method.

--- Person.java
+++ Person.java
@@ -8,27 +8,57 @@ public class Person {
     public void setName(String name){
         this.name = name;
     }
+
     public void setSurname(String surname){
         this.surname = surname;
     }
+
     public void setID(String ID) {
         this.ID = ID;
     }
+
     public void setAlter(int age){
         this.age = age;
     }
-    
+
+    public void setPersonData(String ID, String name, String surname, int age){
+        this.ID = ID;
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+
+
     //Get Methods
     public int getAge() {
         return this.age;
     }
+
     public String getName(){
         return this.name;
     }
+
     public String getSurname(){
         return this.surname;
     }
+
     public String getID(){
         return this.ID;
+    }
+
+
+    // Constructors
+    public Person(String ID){
+        this.ID = ID;
+    }
+
+    public Person(String ID, String name){
+        this(ID);
+        this.name = name;
+    }
+    
+    public Person(String ID, String name, String surname){
+        this(ID, name);
+        this.surname = surname;
     }
 }
