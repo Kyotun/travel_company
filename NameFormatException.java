f98e686cfe5b28c171d0b667c68ea33ac73f0824
Kyotun <emirpisirici@hotmail.com>
2024-03-25 17:23:52 +0100

    NameFormatException class: Added attribute, constructor, set&get.

--- NameFormatException.java
+++ NameFormatException.java
@@ -1,2 +1,9 @@
-public class NameFormatException {
+public class NameFormatException extends RuntimeException{
+    private String message;
+
+    public NameFormatException(String msg){super(msg);}
+
+    public void setMessage(String msg){this.message = msg;}
+    public String getMessage(){return message;}
+
 }
