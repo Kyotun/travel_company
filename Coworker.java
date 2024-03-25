95c2e83dd7e1456c768738744e5bf7356aec06f0
Kyotun <emirpisirici@hotmail.com>
2024-03-25 16:15:47 +0100

    Coworker class: Added attribute (user_number) and its set&get method.

--- Coworker.java
+++ Coworker.java
@@ -1,2 +1,13 @@
 public class Coworker extends Person{
+    private String user_number= "";
+
+    //Get Methods
+    public String getUsernumber(){
+        return this.user_number;
+    }
+
+    //Set Methods
+    public void setUsernumber(String user_number){
+        this.user_number= user_number;
+    }
 }
