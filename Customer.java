c70c545acc0ec2b1717d872b290b82c3a3030063
Kyotun <emirpisirici@hotmail.com>
2024-03-25 18:21:03 +0100

    Customer class: Added function -> printInfo, and organized class structure.

--- Customer.java
+++ Customer.java
@@ -1,46 +1,57 @@
-public class Customer extends Person{
+public class Customer extends Person {
 
     private String customer_number = "";
 
-
-    @Override
-    public String toString(){
-        return getCustomerNumber() + ": " + "ID: " + getID() + ", Name: " + getName() + ", Surname: " + getSurname();
-    }
-
-    @Override
-    public boolean equals(Object obj){
-        Customer customer = (Customer) obj;
-        return getID().equals(customer.getID()) && getCustomerNumber().equals(customer.getCustomerNumber());
-    }
-
-
     //Set Methods
-    public void setCustomerNumber(String customer_number){
+    public void setCustomerNumber(String customer_number) {
         this.customer_number = customer_number;
     }
 
     //Get Methods
-    public String getCustomerNumber(){
+    public String getCustomerNumber() {
         return customer_number;
     }
 
+    // Void Methods
+
+    public void printInfo() {
+        System.out.println("Name: " + getName());
+        System.out.println("Surname: " + getSurname());
+        System.out.println("ID: " + getID());
+        System.out.println("Age: " + getAge());
+    }
+
     //Constructors
-    public Customer(String ID){
+    public Customer(String ID) {
         super(ID);
     }
 
-    public Customer(String ID, String customer_number){
+    public Customer(String ID, String customer_number) {
         super(ID);
         this.customer_number = customer_number;
     }
 
-    public Customer(String ID, String name, String surname){
+    public Customer(String ID, String name, String surname) {
         super(ID, name, surname);
     }
 
-    public Customer(String ID, String name, String surname, String customer_number){
+    public Customer(String ID, String name, String surname, String customer_number) {
         super(ID, name, surname);
         this.customer_number = customer_number;
+    }
+
+    // Overrides
+    @Override
+    public String toString() {
+        return getCustomerNumber() + ": " + "ID: " + getID() + ", Name: " + getName() + ", Surname: " + getSurname();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Customer) {
+            Customer customer = (Customer) obj;
+            return getID().equals(customer.getID()) && getCustomerNumber().equals(customer.getCustomerNumber());
+        }
+        return false;
     }
 }
