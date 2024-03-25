b0cf7401e1f4a9da6f452210412d3e83cab3ed81
Kyotun <emirpisirici@hotmail.com>
2024-03-25 17:21:15 +0100

    Person class: Create a section for class methods and overrides.

--- Person.java
+++ Person.java
@@ -7,58 +7,14 @@ public abstract class Person {
     private int age = 0;
     private  boolean authorisation;
 
-    // Void Methods
-    public void printInfo(){
-        System.out.println("Name: " + getName());
-        System.out.println("Surname: " + getSurname());
-        System.out.println("ID: " + getID());
-    }
-
-    public static void printSize(ArrayList<? extends Person> list){
-        System.out.println(list.size());
-    }
-
-    // Bool Methods
-
-    @Override
-    public boolean equals(Object obj){
-        Person p = (Person) obj;
-        return getID().equals(p.getID());
-    }
-
-    public static boolean idController(String id){
-        return id.charAt(0) != '0' && Character.isDigit(id.charAt(0)) && Character.isDigit(id.charAt(1)) && Character.isDigit(id.charAt(2)) && Character.isDigit(id.charAt(3))
-                && Character.isDigit(id.charAt(4)) && Character.isDigit(id.charAt(5)) && Character.isDigit(id.charAt(6)) && Character.isDigit(id.charAt(7)) &&
-                Character.isDigit(id.charAt(8)) && Character.isDigit(id.charAt(9)) && Character.isDigit(id.charAt(10));
-    }
-
-    public static boolean nameController(String n){
-        int l = n.length();
-        if(l > 2){
-            for(int i = 0; i < l; i++){
-                char p = n.charAt(i);
-                if ( p == ' ' || p == '.' || (p <= 90 && p >= 65) || (p <= 122 && p >= 97) ){
-                    continue;
-                } else {
-                    System.out.println("Please give just whitespaces and letters.");
-                    return false;
-                }
-            }
-            return true;
-        } else{
-            System.out.println("Entry should be longer than 2 characters.");
-            return false;
-        }
-    }
-
     //Set Methods
     public void setName(String name){
-        if(nameController(name)){
+        if(checkName(name)){
             this.name = name;
         }
     }
     public void setSurname(String surname){
-        if(nameController(name)){
+        if(checkName(name)){
             this.surname = surname;
         }
     }
@@ -97,4 +53,39 @@ public abstract class Person {
         this(ID, name);
         this.surname = surname;
     }
+
+    // Class methods
+    public static void printData(Person p){System.out.println(p.getID() + ", " + p.getName() + ", " + p.getSurname());}
+    public static void printSize(ArrayList<? extends Person> list){System.out.println(list.size());}
+    public static boolean idController(String id){
+        return id.charAt(0) != '0' && Character.isDigit(id.charAt(0)) && Character.isDigit(id.charAt(1)) && Character.isDigit(id.charAt(2)) && Character.isDigit(id.charAt(3))
+                && Character.isDigit(id.charAt(4)) && Character.isDigit(id.charAt(5)) && Character.isDigit(id.charAt(6)) && Character.isDigit(id.charAt(7)) &&
+                Character.isDigit(id.charAt(8)) && Character.isDigit(id.charAt(9)) && Character.isDigit(id.charAt(10));
+    }
+    public static boolean checkName(String n){
+        int l = n.length();
+        if(l > 2){
+            for(int i = 0; i < l; i++){
+                char p = n.charAt(i);
+                if ( p == ' ' || p == '.' || (p <= 90 && p >= 65) || (p <= 122 && p >= 97) ){
+                    continue;
+                } else {
+                    System.out.println("Please give just whitespaces and letters.");
+                    return false;
+                }
+            }
+            return true;
+        } else{
+            System.out.println("Entry should be longer than 2 characters.");
+            return false;
+        }
+    }
+
+    // Overrides
+    @Override
+    public boolean equals(Object obj){
+        Person p = (Person) obj;
+        return getID().equals(p.getID());
+    }
+
 }
