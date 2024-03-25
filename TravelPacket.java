e78104b47d865f912c06155488a6aa8e45087a54
Kyotun <emirpisirici@hotmail.com>
2024-03-25 18:46:55 +0100

    TravelPacket class: Added attributes and their set&get methods.

--- TravelPacket.java
+++ TravelPacket.java
@@ -1,2 +1,22 @@
 public class TravelPacket {
+    // Object attributes
+    private String packetID = "";
+    private String destination = "";
+    private String date = "";
+    private int duration = 0;
+    private float price = 0;
+
+    // Set methods
+    public void setPacketID(String packetID){this.packetID = packetID;}
+    public void setDestination(String destination){this.destination= destination;}
+    public void setDatum(String date){this.date = date;}
+    public void setDuration(int duration){this.duration = duration;}
+    public void setPrice(float price){this.price = price;}
+
+    //Get methods
+    public String getPacketID(){return this.packetID;}
+    public String getDestination(){return this.destination;}
+    public String getDatum(){return this.date;}
+    public int getDuration(){return this.duration;}
+    public float getPrice(){return this.price;}
 }
