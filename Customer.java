8eb578ad910d1888b5ee19cdc1db76f0907044dd
Kyotun <emirpisirici@hotmail.com>
2024-03-25 16:32:37 +0100

    Customer class: Added attribute and its set&get method.

--- Customer.java
+++ Customer.java
@@ -1,2 +1,12 @@
 public class Customer {
+    private String customer_number = "";
+
+    //Set Methods
+    public void setCustomerNumber(String customer_number){
+        this.customer_number = customer_number;
+    }
+    //Get Methods
+    public String getCustomerNumber(){
+        return customer_number;
+    }
 }
